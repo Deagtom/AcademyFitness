caabb5e6dda66da179ce1d637373f99d8ce83697
Dagir <dagir.ram@mail.ru>
2023-05-14 01:32:57 +0300

    Delete usings

--- WpfApp/RegistrationCourseWindow.xaml.cs
+++ WpfApp/RegistrationCourseWindow.xaml.cs
@@ -1,22 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// Логика взаимодействия для RegistrationCourseWindow.xaml
-    /// </summary>
     public partial class RegistrationCourseWindow : Window
     {
         AcademyFitnessRamazanovEntities context;
