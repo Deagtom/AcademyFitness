1ceefe686de8116120a3ae1f192e65c221fd765a
Dagir <dagir.ram@mail.ru>
2023-05-14 01:30:19 +0300

    Добавьте файлы проекта.

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AcademyFitnessRamazanovEntities context;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new AcademyFitnessRamazanovEntities();
+            RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+            ComboBoxSelectTrainer.ItemsSource = context.Trainers.ToList();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var newRegistration = new CourseRegistration();
+            context.CourseRegistrations.Add(newRegistration);
+            var window = new RegistrationCourseWindow(context, newRegistration);
+            window.ShowDialog();
+            RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var row = RegistrationGrid.SelectedItem as CourseRegistration;
+            if (row == null)
+            {
+                MessageBox.Show("Строка не выбрана");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Вы уверены?", "Вопрос", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                context.CourseRegistrations.Remove(row);
+                context.SaveChanges();
+                RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+            }
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button EditButton = sender as Button;
+            var currentRegistration = EditButton.DataContext as CourseRegistration;
+            var window = new RegistrationCourseWindow(context, currentRegistration);
+            window.ShowDialog();
+        }
+
+        private void FiltrButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationGrid.ItemsSource = context.CourseRegistrations.Where(x => x.IsDone == true).ToList();
+            MessageBoxResult result = MessageBox.Show("Вернуть все записи?", "Вопрос", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+            }
+        }
+
+        private void ComboBoxSelectTrainer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxSelectTrainer.SelectedItem == null)
+            {
+                return;
+            }
+            var currentTrainer = (Trainer)ComboBoxSelectTrainer.SelectedItem;
+            List<CourseRegistration> listTrainer = context.CourseRegistrations.ToList();
+            RegistrationGrid.ItemsSource = listTrainer.Where(x => x.Trainer == currentTrainer).ToList();
+        }
+
+        private void CanselButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+        }
+    }
+}
---
+++ WpfApp/RegistrationCourseWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationCourseWindow.xaml
+    /// </summary>
+    public partial class RegistrationCourseWindow : Window
+    {
+        AcademyFitnessRamazanovEntities context;
+
+        public RegistrationCourseWindow(AcademyFitnessRamazanovEntities context, CourseRegistration currentRegistration)
+        {
+            InitializeComponent();
+            this.context = context;
+            ComboBoxCourse.ItemsSource = context.Courses.ToList();
+            ComboBoxTrainer.ItemsSource = context.Trainers.ToList();
+
+            this.DataContext = currentRegistration;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckRegistration())
+            {
+                context.SaveChanges();
+                this.Close();
+            }
+        }
+
+        private bool CheckRegistration()
+        {
+            var registration = this.DataContext as CourseRegistration;
+            if (registration.Trainer == null)
+            {
+                MessageBox.Show("Тренер не выбран");
+                return false;
+            }
+            if (registration.Course == null)
+            {
+                MessageBox.Show("Курсы не выбраны");
+                return false;
+            }
+            if (registration.CreatedDate == null)
+            {
+                MessageBox.Show("Дата не выбрана");
+                return false;
+            }
+            if (!int.TryParse(TextTotalPoint.Text, out int result))
+            {
+                MessageBox.Show("Неверные данные");
+                return false;
+            }
+            return true;
+        }
+    }
+}
